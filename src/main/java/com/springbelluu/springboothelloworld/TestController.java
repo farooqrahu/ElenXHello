f6bcec836c59b05e86cdd2fb1002faaa32bacaae
Bartłomiej Komendarczuk <bkomendarczuk@gmail.com>
2018-07-14 18:52:55 +0200

    localhost:8080/welcome.html

--- src/main/java/com/springbelluu/springboothelloworld/TestController.java
+++ src/main/java/com/springbelluu/springboothelloworld/TestController.java
@@ -1,4 +1,4 @@
-package com.javainuse.controllers;
+package com.springbelluu.springboothelloworld;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
