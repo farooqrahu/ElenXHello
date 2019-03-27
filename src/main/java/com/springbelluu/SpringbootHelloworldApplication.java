2a0f516d3cae53a7ebbe6b924213f8282a28ba5f
Farooq Rahu <farooqrahu@hotmail.com>
2019-03-27 11:55:09 +0500

    Issue fixed with one controller. Only added controller package in  @ComponentScan annotation.

--- src/main/java/com/springbelluu/SpringbootHelloworldApplication.java
+++ src/main/java/com/springbelluu/SpringbootHelloworldApplication.java
@@ -1,11 +1,12 @@
-package com.springbelluu.springboothelloworld;
+package com.springbelluu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"com.javainuse.controllers"})
 public class SpringbootHelloworldApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootHelloworldApplication.class, args);
 	}
